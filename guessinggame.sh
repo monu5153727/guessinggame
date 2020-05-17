3110c0583dbe17b74a9faeca16def488e24dcfcf
tripura1234 <65503373+tripura1234@users.noreply.github.com>
2020-05-18 00:39:32 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,15 +1,32 @@
-The guessinggame.sh program should have the following behavior:
 
-When the program starts the user should be asked how many files are in the current directory, and then the user should be prompted for a guess.
-If the user's answer is incorrect the user should be advised that their guess was either too low or too high and then they should be prompted to try to guess again.
-If the user's guess is correct then they should be congratulated and the program should end.
-The program should not end until the user has entered the correct number of files in the current directory.
-The program should be able to be run by entering bash guessinggame.sh into the console.
-The program should contain at least one function, one loop, and one if statement.
-The program should be more than 20 lines of code but less than 50 lines of code.
 
-#bin/bash
+#!/bin/bash
 
-echo guess how many file in the current directory
+function filespresent()
+{
+  ls| wc -l
+}
 
+echo guess how  many files in this directory
+
+read num
+
+
+
+ if [[ $num -eq 3 ]]
+ then 
+  echo 'congrats!Guess  is actually correct'
+ elif [[ $num -lt 3 ]]
+ then
+echo 'oops! guess is lower'
+
+elif [[ $num -gt 3 ]]
+ then
+echo 'oops! guess is higher'
+
+
+fi
+
+echo 'actual presnt file'
+filespresent
 
