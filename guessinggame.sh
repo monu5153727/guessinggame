bb270f934ae74e9418a151435194ec7da24991c3
tripura1234 <65503373+tripura1234@users.noreply.github.com>
2020-05-18 00:43:13 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,6 +1,7 @@
 
-
 #!/bin/bash
+
+
 
 function filespresent()
 {
@@ -12,14 +13,17 @@ echo guess how  many files in this directory
 read num
 
 
-
+#check if guess is correct
  if [[ $num -eq 3 ]]
  then 
   echo 'congrats!Guess  is actually correct'
+
+  #if it guess is low
  elif [[ $num -lt 3 ]]
  then
 echo 'oops! guess is lower'
 
+ #if it guess is high
 elif [[ $num -gt 3 ]]
  then
 echo 'oops! guess is higher'
@@ -29,4 +33,3 @@ fi
 
 echo 'actual presnt file'
 filespresent
-
